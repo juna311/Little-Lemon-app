ef2819e8c8b976ce3a42c0819dcc33f357fb9f13
Yuna <yuna@Yuliias-MacBook-Air.local>
2025-07-16 00:49:45 +0900

    Updated Header and Footer

--- src/Footer.js
+++ src/Footer.js
@@ -1,5 +1,34 @@
+import logo from './assets/Small-logo.png'
+
 export default function Footer(){
     return (
-        <footer>Footer</footer>
+       <footer>
+            <div>
+                <img src={logo} alt="Little Lemon logo" />
+            </div>
+            <div>
+                <nav aria-label="Doormat navigation">
+                    <ul>
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">About</a></li>
+                        <li><a href="#">Menu</a></li>
+                        <li><a href="#">Reservations</a></li>
+                        <li><a href="#">Order Online</a></li>
+                        <li><a href="#">Login</a></li>
+                    </ul>
+                </nav>
+            </div>
+            <div>
+                <address>Address</address>
+            </div>
+            <div>
+                <ul>
+                    <li><a href="#">Facebook</a></li>
+                    <li><a href="#">Instagram</a></li>
+                    <li><a href="#">Twitter</a></li>
+                    <li><a href="#">YouTube</a></li>
+                </ul>
+            </div>
+       </footer>
     );
 }
--- src/Header.js
+++ src/Header.js
@@ -1,9 +1,18 @@
+import logo from './assets/Logo-lemon.jpg';
+
 export default function Header(){
     return (
         <header>
-            <nav>
-                <img alt="logo"></img>
-                <ul></ul>
+            <nav aria-label="Main navigation">
+                <img src={logo} alt="Little Lemon logo" />
+                <ul>
+                    <li><a href='#'>Home</a></li>
+                    <li><a href='#'>About</a></li>
+                    <li><a href='#'>Menu</a></li>
+                    <li><a href='#'>Reservations</a></li>
+                    <li><a href='#'>Order Online</a></li>
+                    <li><a href='#'>Login</a></li>
+                </ul>
             </nav>
         </header>
     )
