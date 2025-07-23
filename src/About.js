a59f911969c8ea1bc21e3fffca3e9c04a5d0ed96
Yuna <yuna@Yuliias-MacBook-Air.local>
2025-07-24 00:01:06 +0900

    About section

--- src/About.js
+++ src/About.js
@@ -1,5 +1,18 @@
+import interior from "./assets/restaurantInterior.jpg"
+import food from "./assets/food.jpg"
+
 export default function About(){
     return (
-        <section>About the restaurant</section>
+        <section className="aboutSection">
+            <div className="aboutText">
+                <h3>Little Lemon</h3>
+                <h4>Chicago</h4>
+                <p>Some description text</p>
+            </div>
+            <div className="aboutImg">
+                <img src={interior} alt="Little Lemon restaurant interior" />
+                <img src={food} alt="Mediterranean dish served at Little Lemon" />
+            </div>
+        </section>
     );
 }
